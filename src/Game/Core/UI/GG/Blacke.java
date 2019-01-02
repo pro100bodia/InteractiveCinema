d462e9ae272fbb11cac805a59ae668c9bd4ae63c
Nannik <45785195+Nannik@users.noreply.github.com>
2019-01-02 10:44:28 +0200

    Add files via upload

--- src/Game/Core/UI/GG/Blacke.java
+++ src/Game/Core/UI/GG/Blacke.java
@@ -1,19 +1,27 @@
 package Game.Core.UI.GG;
 
+import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 import org.newdawn.slick.*;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.state.StateBasedGame;
 
 
 public class Blacke extends BasicGame implements MouseListener {
-	
+	private float x;
+	private float y;
+	private float moveTo;
+	private float twiceX;
+	private float twiceMove;
+
 	private Image ggImageLeft ;
 	private Image ggImageRight ;
 	private Image  ggImage;
-	private float x, y, moveTo;
-	GameContainer gameContainer;
-	
+
+	private GameContainer gameContainer;
+
 	public Blacke(int x, String title, GameContainer gameContainer) {
 		super("GG");
 		this.gameContainer=gameContainer;
@@ -42,49 +50,74 @@ public class Blacke extends BasicGame implements MouseListener {
     
     @Override
     public void render(GameContainer gameContainer, Graphics g) throws SlickException {
-    	ggImage.draw(x,y);
+    	ggImage.draw(x + twiceX,y);
     }
+
     @Override
     public void update(GameContainer gameContainer, int t) throws SlickException {
-    	
-    	if(x < moveTo) {
+
+		if(x < moveTo) { // движение в правую сторону
 			if (moveTo - x <= 5)
 				this.x = moveTo;
 			else
 				this.x += 5;
 		}
 
-    	if(x > moveTo){
-        	if (x - moveTo <= 5)
-        		x = moveTo;
-        	else
-    			this.x -=5;
-    	}
+		if(x > moveTo){ // движение в левую сторону
+			if (x - moveTo <= 5)
+				x = moveTo;
+			else
+				this.x -=5;
+		}
+
+		if(twiceX < twiceMove){
+			if (twiceMove - twiceX <= 5)
+				twiceX = twiceMove;
+			else
+				twiceX += 5;
+		}
+
+		if(twiceX > twiceMove){
+			if (twiceX - twiceMove <= 5)
+				twiceX = twiceMove;
+			else
+				twiceX -= 5;
+		}
     	
     }
     
     public void moveTo(float x) {
     		moveTo = x;
-    		
-    		if(moveTo > this.x)
-    		{
-    			this.ggImage = this.ggImageRight;
-    		}
-    		
-    		if(moveTo < this.x)
-    		{
-    			this.ggImage = this.ggImageLeft;
-    		}
-    	
-    	}
 
-    public float getX() {
+		if (moveTo + twiceMove > this.x + twiceX){
+			ggImage = ggImageRight;
+		}
+
+		if (moveTo + twiceMove < this.x + twiceX){
+			ggImage = ggImageLeft;
+		}
+	}
+
+	public float getX() {
 		return this.x;
 	}
     public int getWidth(){
 		return ggImage.getWidth();
 	}
+	public int getMoveTo(){
+    	return (int) moveTo;
+	}
     public void setX(float x) {
     	this.x = x;
+    }
+    public void setTwiceMove (float move){
+		twiceMove = move;
+	}
+	public void setDirection (String direction){
+    	if (direction == "left"){
+			ggImage = ggImageLeft;
+		}else {
+    		ggImage = ggImageRight;
+		}
     }
 }
